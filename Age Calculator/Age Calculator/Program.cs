0b273d352aef19a39e3f6728a2889f8c75ca49e1
Kavya Sri Chokka <140721147+KavyaSriChokka@users.noreply.github.com>
2025-07-05 15:06:54 +0530

    Add files via upload

---
+++ Age Calculator/Age Calculator/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Data;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter Date of birth(dd-mm-yyyy):");
+        string? input = Console.ReadLine(); ;
+        if (!string.IsNullOrWhiteSpace(input))
+        {
+            DateTime dob = DateTime.Parse(input);
+            int years = DateTime.Now.Year - dob.Year;
+            int months = DateTime.Now.Month - dob.Month;
+            int days = DateTime.Now.Day - dob.Day;
+
+            TimeSpan time = DateTime.Now - dob;
+
+            if (days < 0)
+            {
+                months--;
+                days += DateTime.DaysInMonth(DateTime.Now.Year, (DateTime.Now.Month == 1 ? 12 : (DateTime.Now.Month - 1)));
+            }
+            if (months < 0)
+            {
+                years--;
+                months += 12;
+            }
+            Console.WriteLine($"{years}years, {months}months, {days}days");
+            Console.WriteLine($"{time.Hours}hours:{time.Minutes}minutes:{time.Seconds}seconds");
+        }
+        else
+        {
+            Console.WriteLine("Enter valid date of birth");
+        }
+    }
+}
